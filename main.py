4e15c82b38f79887d879482e114ccad616f0f875
MayVCODE <114690973+Arustat@users.noreply.github.com>
2025-03-18 17:14:58 +0100

    changement json

---
+++ main.py
@@ -0,0 +1,12 @@
+import json
+
+#Programme principale
+def main():
+    
+
+#Fonction Ajouter un mot
+def ajouter_mot():
+    f = open("mot_disco.csv","w")
+    print("Vous voulez ajouter un mot !")
+    mot = input("Veuillez entrer votre mot : ")   
+    
