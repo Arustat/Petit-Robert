aece8dc81d8e50c5f9475a5d2e9c62c08d66732c
MayVCODE <114690973+Arustat@users.noreply.github.com>
2025-03-19 08:41:02 +0100

    mpo

--- main.py
+++ main.py
@@ -4,10 +4,3 @@ import json
 def main():
     
 
-#Fonction Ajouter un mot
-def ajouter_mot():
-    print("Vous voulez ajouter un mot !")
-    mot = input("Veuillez entrer votre mot : ")
-    f = open("mot_disco.json","r")
-    contenu = f.read()
-    for i in 
