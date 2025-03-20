b88d9fc453191391c659d5d42f53a7aad4c721af
nexus806 <antonin.merlomeyffren@ynov.com>
2025-03-20 14:10:20 +0100

    test pour changement

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+
+
+def suppression():
+    sup_request = input("quel mots voulez vous suprimez du dictionnaire : ")
+    x = list()
+    y = list()
+    with open("mot_dico.csv","r") as dico:
+        for line in dico:
+            if "#" in line:
+                # on saute la ligne continue
+                continue
+            data=line.split()
+            x.append(data[0])
+            y.append(data[1])
+    print(x)
+    print(y)
+        
+                
+            
+
+suppression()
