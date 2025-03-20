b019b58ee3d6457465b453f6e3b2755517908403
MayVCODE <114690973+Arustat@users.noreply.github.com>
2025-03-20 15:13:06 +0100

    re

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import json
+# Description: Programme principale du jeu 
+# Auteur: Mayssa / Antonin / Raphael
+# Date: 07/10/2020
+# Licence: GNU GPL v3
+# Fonction Ajouter un mot
 
 #Programme principale
 def main():
