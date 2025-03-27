d4193476667644116152af730387b2174d15e15f
nexus806 <antonin.merlomeyffren@ynov.com>
2025-03-27 22:06:47 +0100

    updat

--- main.py
+++ main.py
@@ -4,22 +4,4 @@
 # Licence: GNU GPL v3
 # Fonction Ajouter un mot
 
-import re
 
-
-
-def suppression():
-    sup_request = input("Quel mot voulez-vous supprimer du dictionnaire ? ")
-
-    # Lire toutes les lignes du fichier
-    with open("mot_dico.txt", "r") as file:
-        lines = file.readlines()
-
-    # Ouvrir le fichier en écriture après la lecture
-    with open("mot_dico.txt", "w") as file:
-        for line in lines:
-            if not re.search(rf'\b{re.escape(sup_request)}\b', line):
-                file.write(line)
-
-
-suppression()
