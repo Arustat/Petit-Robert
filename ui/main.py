753ed24a43885d69e2275f3c8c199f0c7d41bcd2
MayVCODE <mayssou839@gmail.com>
2025-04-03 17:03:12 +0200

    ui button

--- ui/main.py
+++ ui/main.py
@@ -13,7 +13,7 @@ screen_height = root.winfo_screenheight()
 
 # Redimensionnement de l'image de fond
 original_bg = Image.open('assets/bg.png')
-resized_bg = original_bg.resize((width, height), Image.ANTIALIAS)  # Redimensionne l'image
+resized_bg = original_bg.resize((width, height), Image.Resampling.LANCZOS)  # Redimensionne l'image
 bg = ImageTk.PhotoImage(resized_bg)
 
 bg_label = tk.Label(root, image=bg)
@@ -24,6 +24,25 @@ y = (screen_height // 2) - (height // 2)
 
 root.geometry(f"{width}x{height}+{x}+{y}")
 
+root.resizable(False, False)
+
 root.iconbitmap("assets/dictionary_icon.ico")
+
+start_button = tk.Button(
+    root,
+    text="Start",
+    command = main()
+)
+exit_button = tk.Button(
+    root,
+    text="Exit",
+    command = root.quit()
+)
+
+start_button = tk.Button(root, text="Start", width=20, height=2)
+start_button.place(x=230, y=500)
+
+exit_button = tk.Button(root, text="Exit", width=20, height=2)
+exit_button.place(x=230, y=550)
 
 root.mainloop()
